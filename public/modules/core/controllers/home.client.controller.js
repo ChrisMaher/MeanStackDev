15143a61982c628e0a69d5241d2e92d0f95b474f
Chris Maher <chrismaher.wit@gmail.com>
2015-07-11 10:36:40 +0100

    Home Screen Deals.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -10,58 +10,123 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
 			{
 
-			icon : 'glyphicon-user',
+			icon : 'glyphicon-euro',
 			colour: 'btn-success',
-			total: '20,408',
-			description: 'Total Customers'
+			total: '12',
+			description: 'New Deals in 24h'
 
 			},
 
             {
 
-                icon : 'glyphicon-calendar',
-                colour: 'btn-primary',
-                total: '8,382',
-                description: 'Upcoming Events'
-
-            },
-
-            {
-
-                icon : 'glyphicon-edit',
+                icon : 'glyphicon-euro',
                 colour: 'btn-success',
-                total: '20,527',
-                description: 'New Customers in 24h'
+                total: '596',
+                description: 'Total Deals'
 
             },
 
             {
 
-                icon : 'glyphicon-record',
-                colour: 'btn-info',
-                total: '23,000',
-                description: 'Emails Sent'
+                icon : 'glyphicon-user',
+                colour: 'btn-default',
+                total: '56',
+                description: 'New Members in 24h'
 
             },
 
             {
 
-                icon : 'glyphicon-eye-open',
+                icon : 'glyphicon-user',
+                colour: 'btn-default',
+                total: '958',
+                description: 'Total Members'
+
+            },
+
+            {
+
+                icon : 'glyphicon-comment',
                 colour: 'btn-warning',
-                total: '568',
-                description: 'Follow Ups Required'
+                total: '98',
+                description: 'New Comments in 24h'
 
             },
 
             {
 
-                icon : 'glyphicon-flag',
-                colour: 'btn-danger',
-                total: '348',
-                description: 'Referrals To Moderate'
+                icon : 'glyphicon-comment',
+                colour: 'btn-warning',
+                total: '1,254',
+                description: 'Total Comments'
 
             },
 
 		];
 	}
+]);
+
+// Deals controller
+angular.module('deals').controller('DealsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Deals',
+    function($scope, $stateParams, $location, Authentication, Deals) {
+        $scope.authentication = Authentication;
+
+        // Create new Deal
+        $scope.create = function() {
+            // Create new Deal object
+            var deal = new Deals ({
+                name: this.name
+            });
+
+            // Redirect after save
+            deal.$save(function(response) {
+                $location.path('deals/' + response._id);
+
+                // Clear form fields
+                $scope.name = '';
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        // Remove existing Deal
+        $scope.remove = function(deal) {
+            if ( deal ) {
+                deal.$remove();
+
+                for (var i in $scope.deals) {
+                    if ($scope.deals [i] === deal) {
+                        $scope.deals.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.deal.$remove(function() {
+                    $location.path('deals');
+                });
+            }
+        };
+
+        // Update existing Deal
+        $scope.update = function() {
+            var deal = $scope.deal;
+
+            deal.$update(function() {
+                $location.path('deals/' + deal._id);
+            }, function(errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        // Find a list of Deals
+        $scope.find = function() {
+            $scope.deals = Deals.query();
+        };
+
+        // Find existing Deal
+        $scope.findOne = function() {
+            $scope.deal = Deals.get({
+                dealId: $stateParams.dealId
+            });
+        };
+    }
 ]);
